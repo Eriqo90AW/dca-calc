a0345d83444a5a9bd0064e5490231c0013ec2eea
Eriqo90AW <eriqo.arief@ui.ac.id>
2024-03-30 12:26:42 +0700

    fix static

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use("/", express.static("public"));
 app.use(cors());
-
-app.use("/test", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.get("/users", (req, res) => {
   const data = [
