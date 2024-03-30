bfe02b4595484bcffe94780b97e1ea3113cbfd72
Eriqo90AW <eriqo.arief@ui.ac.id>
2024-03-30 12:37:24 +0700

    img bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.get('/', async(req, res) => {
