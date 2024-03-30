61c479737466d12c28fc09b84ff9c13c65344c7d
Eriqo90AW <eriqo.arief@ui.ac.id>
2024-03-30 12:46:06 +0700

    bug fixing

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.get('/', async(req, res) => {
